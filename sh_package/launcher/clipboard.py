4714c32e55385d6344e13d4143557bbacc52698f
Yu Wang <yuwan@microsoft.com>
2022-03-30 22:01:17 -0700

    feat: add set_clipboard_text

--- sh_package/launcher/clipboard.py
+++ sh_package/launcher/clipboard.py
@@ -8,3 +8,8 @@ def get_clipboard_text():
     text = win32clipboard.GetClipboardData(win32con.CF_UNICODETEXT)
     win32clipboard.CloseClipboard()
     return text
+
+def set_clipboard_text(text: str):
+    win32clipboard.OpenClipboard()
+    win32clipboard.SetClipboardText(text, win32con.CF_UNICODETEXT)
+    win32clipboard.CloseClipboard()
